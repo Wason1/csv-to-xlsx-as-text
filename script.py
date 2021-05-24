24aba335146c2f0be34616d06eae2adc38e281db
Wason1 <jason.j.whittle@gmail.com>
2021-05-24 16:40:27 +1000

    Create script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+file_dir = r'C:\Users\whittlj2\data\input.csv'
+output_dir = r'C:\Users\whittlj2\data\input.csv'
+delimiter = ','
+data_type = 'str'
+
+import pandas as pd
+import numpy as np
+import openpyxl as xl
+
+df_file = pd.read_csv(
+    file_dir
+    , sep = delimiter
+    , dtype=data_type
+    , engine = 'python')
+
+
+writer = pd.ExcelWriter(output_dir, engine = 'openpyxl')
+df_file.to_excel(writer, index = False)
